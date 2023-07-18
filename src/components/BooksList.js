f20419b3ec11fa82e4bf7b3f84e41a144e676b6d
Muhammad Amran Khan (Mike) <Tipu870@gmail.com>
2023-07-18 14:44:16 +0500

    Update BooksList.js

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -1,40 +1,12 @@
 import React from 'react';
-import AddBook from './AddBook';
 import BookCard from './BookCard';
+import AddBook from './AddBook';
 
-const Books = () => {
-  const books = [{
-    id: '1',
-    category: 'Action',
-    title: 'The Hunger Games',
-    author: 'Suzanne Collins',
-    progres: '63%',
-    currentChapter: 'CURRENT CHAPTER',
-    chapterNumber: 'Chapter17: *Get Busy Living*',
-  },
-  {
-    id: '2',
-    category: 'Science Fiction',
-    title: 'Dune',
-    author: 'Frank Herbert',
-    progres: '18%',
-    currentChapter: 'CURRENT CHAPTER',
-    chapterNumber: 'Chapter3: *A Lesson Learned*',
-  },
-  {
-    id: '3',
-    category: 'Economy',
-    title: 'Capital in the Twenty-First Century',
-    author: 'Suzanne Collins',
-    progres: '20%',
-    currentChapter: 'CURRENT CHAPTER',
-    chapterNumber: 'Chapter2: *Introduction To World Order*',
-  }];
-  return (
-    <div>
-      <BookCard key={books.id} book={books} />
-      <AddBook />
-    </div>
-  );
-};
+const Books = () => (
+  <>
+    <BookCard />
+    <AddBook />
+
+  </>
+);
 export default Books;
