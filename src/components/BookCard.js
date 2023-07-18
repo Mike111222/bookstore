e382b25dc2790c960d1a6a27588ec5121bce50f4
Muhammad Amran Khan (Mike) <Tipu870@gmail.com>
2023-07-18 14:43:18 +0500

    Update BookCard.js

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,45 +1,43 @@
 import React from 'react';
-// eslint-disable-next-line import/no-extraneous-dependencies
-import Proptypes from 'prop-types';
+import { useSelector, useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/booksSlice';
 
-const BookCard = ({ book }) => {
-  const renderBooks = book.map((book) => (
-    <>
-      <div className="bookContainer">
-        <div className="bookTitle">
-          <p>{ book.category }</p>
-          <p>{ book.title }</p>
-          <p>{ book.author }</p>
-          <ul className="buttonsList">
-            <li><button type="button">Comments</button></li>
-            <li><button type="button">Remove</button></li>
-            <li><button type="button">Edit</button></li>
-          </ul>
-        </div>
-        <div className="bookChart">
-          <div>Chart</div>
-          <div>
-            <p>{ book.progres }</p>
-            <p>COMPLETED</p>
-          </div>
-        </div>
-        <div className="bookChapter">
-          <p>{ book.currentChapter }</p>
-          <p>{ book.chapterNumber }</p>
-          <button type="button">Update Progress</button>
+const BookCard = () => {
+  const books = useSelector((store) => store.books.books);
+  const dispatch = useDispatch();
+
+  const renderBooks = books.map((book) => (
+    <div key={book.item_id} className="bookContainer">
+      <div className="bookTitle">
+        <p>{book.category}</p>
+        <p>{book.title}</p>
+        <p>{book.author}</p>
+        <ul className="buttonsList">
+          <li><button type="button">Comments</button></li>
+          <li><button type="button" onClick={() => dispatch(removeBook(book.item_id))}>Remove</button></li>
+          <li><button type="button">Edit</button></li>
+        </ul>
+      </div>
+      <div className="bookChart">
+        <div>Chart</div>
+        <div>
+          <p>progres</p>
+          <p>COMPLETED</p>
         </div>
       </div>
-    </>
+      <div className="bookChapter">
+        <p>currentChapter</p>
+        <p>chapterNumber</p>
+        <button type="button">Update Progress</button>
+      </div>
+    </div>
   ));
+
   return (
     <div className="mainContainer">
-      { renderBooks }
+      {renderBooks}
     </div>
   );
-};
-
-BookCard.propTypes = {
-  book: Proptypes.arrayOf(Proptypes.shape({})).isRequired,
 };
 
 export default BookCard;
