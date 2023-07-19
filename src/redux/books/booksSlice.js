6f8bedb43e0da9cecaafd91c17ecb8969946d201
Muhammad Amran Khan (Mike) <Tipu870@gmail.com>
2023-07-19 20:52:20 +0500

    Update booksSlice.js

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,9 +1,20 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
+
 // API URL
 const booksAPI = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/UUD89QFWkJvMJew3gPgt';
 
-// Async thunk for adding a book
+export const fetchBooks = createAsyncThunk('books/fetchBooks', async () => {
+  const response = await axios.get(`${booksAPI}/books`);
+  const books = Object.entries(response.data).map((item) => (
+    {
+      ...item[1][0],
+      item_id: item[0],
+    }
+  ));
+  return books;
+});
+
 export const addBook = createAsyncThunk('books/addBook', async (book) => {
   try {
     await axios.post(`${booksAPI}/books`, book);
@@ -22,18 +33,6 @@ export const deleteBook = createAsyncThunk('books/deleteBook', async (bookId) =>
   return bookId;
 });
 
-export const getBooks = createAsyncThunk('books/getBooks', async () => {
-  const response = await axios.get(`${booksAPI}/books`);
-
-  const books = Object.entries(response.data).map((item) => (
-    {
-      ...item[1][0],
-      item_id: item[0],
-    }
-  ));
-  return books;
-});
-
 const initialState = {
   books: [],
   isLoading: false,
@@ -46,26 +45,26 @@ const booksSlice = createSlice({
   reducers: {},
 
   extraReducers: (builder) => {
+    builder.addCase(fetchBooks.pending, (state) => {
+      state.isLoading = true;
+    });
+
+    builder.addCase(fetchBooks.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.books = action.payload;
+    });
+
+    builder.addCase(fetchBooks.rejected, (state) => {
+      state.isLoading = false;
+      state.isError = true;
+    });
+
     builder.addCase(addBook.fulfilled, (state, action) => {
       state.books.push(action.payload);
     });
 
     builder.addCase(deleteBook.fulfilled, (state, action) => {
       state.books = state.books.filter((book) => book.item_id !== action.payload);
-    });
-
-    builder.addCase(getBooks.pending, (state) => {
-      state.isLoading = true;
-    });
-
-    builder.addCase(getBooks.fulfilled, (state, action) => {
-      state.isLoading = false;
-      state.books = action.payload;
-    });
-
-    builder.addCase(getBooks.rejected, (state) => {
-      state.isLoading = false;
-      state.isError = true;
     });
   },
 });
