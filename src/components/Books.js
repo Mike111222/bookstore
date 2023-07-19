1c5c38bb39c3c74e155af66619f6a3332695ea51
Muhammad Amran Khan (Mike) <Tipu870@gmail.com>
2023-07-19 09:48:18 +0500

    Create Books.js

---
+++ src/components/Books.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import BookCard from './BookCard';
+import AddBook from './AddBook';
+
+const Books = () => (
+  <>
+    <BookCard />
+    <AddBook />
+
+  </>
+);
+export default Books;
