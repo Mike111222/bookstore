6bb8434f75747be007ac277fc5ddf1f75a2916e9
Mike111222 <Tipu870@gmail.com>
2023-07-20 20:47:09 +0500

    Update booksSlice.js

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -2,9 +2,21 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 // API URL
+// API URL
 const booksAPI = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/UUD89QFWkJvMJew3gPgt';
 
-// Async thunk for adding a book into the list
+// fetching the books from the API.
+export const fetchBooks = createAsyncThunk('books/fetchBooks', async () => {
+  const response = await axios.get(`${booksAPI}/books`);
+  const books = Object.entries(response.data).map((item) => (
+    {
+      ...item[1][0],
+      item_id: item[0],
+    }
+  ));
+  return books;
+});
+// AsyncThunk for adding the books.
 export const addBook = createAsyncThunk('books/addBook', async (book) => {
   try {
     await axios.post(`${booksAPI}/books`, book);
