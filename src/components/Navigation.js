d49d5297687412b220a6f43a241e2837ba3593b4
Mike111222 <Tipu870@gmail.com>
2023-07-20 14:20:54 +0500

    Update Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,16 +2,29 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => (
-  <nav className="navBar">
-    <div className="navItems">
-      <h2>Bookstore CMS</h2>
-      <ul className="navLinks">
-        <li><Link to="/">Books</Link></li>
-        <li><Link to="categories">Categories</Link></li>
-      </ul>
-    </div>
-    <img src="https://img.icons8.com/external-anggara-outline-color-anggara-putra/32/000000/external-avatar-interface-anggara-outline-color-anggara-putra.png" alt="profile" />
-  </nav>
+  <>
+    <nav className="navBar navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <h2 className="navTitle navbar-brand">Bookstore CMS</h2>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarText">
+          <ul className="navLinks navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item me-5">
+              <Link to="/">Books</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="categories">Categories</Link>
+            </li>
+          </ul>
+          <span className="navbar-text">
+            <img className="d-none d-md-none d-lg-block" src="https://img.icons8.com/external-anggara-outline-color-anggara-putra/32/000000/external-avatar-interface-anggara-outline-color-anggara-putra.png" alt="profile" />
+          </span>
+        </div>
+      </div>
+    </nav>
+  </>
 );
 
 export default Navbar;
